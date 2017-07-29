83b13dd7367324a9f321d27e6650349cc28fbf28
willewerton <william_ewerton@outlook.com>
2017-07-29 16:16:35 -0300

    init

---
+++ app/viewmodels/criarconta.js
@@ -0,0 +1,8 @@
+define(function () {
+    var ctor = function () {
+        this.displayName = 'MyTest';
+        this.description = 'Durandal is a cross-device, cross-platform client framework written in JavaScript and designed to make Single Page Applications (SPAs) easy to create and maintain.';
+    };
+
+    return ctor;
+});
